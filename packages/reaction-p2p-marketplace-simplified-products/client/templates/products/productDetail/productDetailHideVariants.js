076efcccd14d3462031b2f7f68e77a96458b0672
ScyDev <lukas.saegesser@gmail.com>
2016-02-17 17:59:39 +0100

    - injecting dynamic changes to productDetail to auto-fill and hide variants

--- packages/reaction-p2p-marketplace-simplified-products/client/templates/products/productDetail/productDetailHideVariants.js
+++ packages/reaction-p2p-marketplace-simplified-products/client/templates/products/productDetail/productDetailHideVariants.js
@@ -4,13 +4,28 @@ Template.productDetailHideVariants.inheritsEventsFrom("productDetail");
 Template.productDetailHideVariants.inheritsHooksFrom("productDetail");
 
 Template.productDetail.rendered = function() {
+  if (!ReactionCore.hasPermission("createProduct")) {
+    // auto-select variant
+    $('.variant-detail .variant-title').click();
+    console.log("auto selected variant");
 
-  console.log("wakka!");
-  // auto-select variant
-  $('.variant-detail').click();
-  console.log("selected variant");
+    // hide variant
+    $('.variant-list-item').hide();
+    console.log("hid variant");
+  }
+  else {
+    var productTitle = $('.title-edit-input').val();
+    $('.variant-edit-form [name=title]').val(productTitle);
+    $('.variant-edit-form [name=weight]').val('1');
 
-  // hide variant
-  //$('.variant-list-item').hide();
-  console.log("hid variant");
+    // click variant edit for seller to see
+
+    // check if it's already open
+    $('.variant-edit').click();
+    console.log("clicked variant edit");
+
+    // prevent second variant
+    $('.variant-form-buttons').hide();
+    console.log("hid variant edit buttons");
+  }
 };
