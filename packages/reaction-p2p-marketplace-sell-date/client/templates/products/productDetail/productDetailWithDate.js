65f56b3d9d37fa672076ebeb7112c48ee95300ab
ScyDev <lukas.saegesser@gmail.com>
2016-02-17 21:44:49 +0100

    - don't simply override rendered callback of a template. always call existing callbacks first.

--- packages/reaction-p2p-marketplace-sell-date/client/templates/products/productDetail/productDetailWithDate.js
+++ packages/reaction-p2p-marketplace-sell-date/client/templates/products/productDetail/productDetailWithDate.js
@@ -1,7 +1,10 @@
 
 //Template.productDetailWithDate.replaces("productDetail"); // not even necessary to act on the event below
-
+var existingRenderedCallback = Template.productDetail.rendered;
 Template.productDetail.rendered = function() {
+  if (existingRenderedCallback != null) existingRenderedCallback();
+
   // inject date field template here
   Blaze.renderWithData(Template.productDetailDateField, this.data, $(".pdp-right-column")[0])
+  console.log('injected date field');
 };
