0994dda38a42659505e65a63b285cd4943d9c463
ScyDev <lukas.saegesser@gmail.com>
2016-02-20 18:41:43 +0100

    - jquery $ was not initialized either when event fired

--- packages/reaction-p2p-marketplace-sell-date/client/templates/products/productDetail/productDetailWithDate.js
+++ packages/reaction-p2p-marketplace-sell-date/client/templates/products/productDetail/productDetailWithDate.js
@@ -5,12 +5,12 @@ Template.productDetail.onCreated(
   function() {
     Meteor.setTimeout(function() { // what the?!? document doesn't seem to be ready immediately when this event is fired...
 
-      if ($('#forSaleOnDate').length === 0) { // make sure its only injected once, not on every rendered event
+      if ($('#forSaleOnDate').length === 0) { // make sure its only injected once, not on every rendered event. should be obsolete with onCreated.
         // inject date field template here
-        console.log('injecting date field into: '+document.querySelectorAll(".pdp-right-column")[0]);
-        Blaze.renderWithData(Template.productDetailDateField, this.data, document.querySelectorAll(".pdp-right-column")[0])
+        console.log('injecting date field into: '+$(".pdp-right-column")[0]);
+        Blaze.renderWithData(Template.productDetailDateField, this.data, $(".pdp-right-column")[0])
         console.log('injected date field');
       }
-    }, 1000);
+    }, 100);
   }
 );
