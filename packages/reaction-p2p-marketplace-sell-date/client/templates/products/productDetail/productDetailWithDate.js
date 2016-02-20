a959db9f1444055f635d3094de9aedbbb0325b16
ScyDev <lukas.saegesser@gmail.com>
2016-02-20 18:39:25 +0100

    - document doesn't seem to be ready immediately when even is fired. very bad...

--- packages/reaction-p2p-marketplace-sell-date/client/templates/products/productDetail/productDetailWithDate.js
+++ packages/reaction-p2p-marketplace-sell-date/client/templates/products/productDetail/productDetailWithDate.js
@@ -1,12 +1,16 @@
 
 //Template.productDetailWithDate.replaces("productDetail"); // not even necessary to act on the event below
 
-Template.productDetail.onRendered(
+Template.productDetail.onCreated(
   function() {
-    if ($('#forSaleOnDate').length === 0) { // make sure its only injected once, not on every rendered event
-      // inject date field template here
-      Blaze.renderWithData(Template.productDetailDateField, this.data, $(".pdp-right-column")[0])
-      console.log('injected date field');
-    }
+    Meteor.setTimeout(function() { // what the?!? document doesn't seem to be ready immediately when this event is fired...
+
+      if ($('#forSaleOnDate').length === 0) { // make sure its only injected once, not on every rendered event
+        // inject date field template here
+        console.log('injecting date field into: '+document.querySelectorAll(".pdp-right-column")[0]);
+        Blaze.renderWithData(Template.productDetailDateField, this.data, document.querySelectorAll(".pdp-right-column")[0])
+        console.log('injected date field');
+      }
+    }, 1000);
   }
 );
