d092d1b1e6e40f985b9ed9f7a7a11e2ac47b2978
ScyDev <lukas.saegesser@gmail.com>
2016-02-21 11:46:17 +0100

    - use products helper from productGrid for produtList too

--- packages/reaction-product-variant/client/templates/products/productList/productList.js
+++ packages/reaction-product-variant/client/templates/products/productList/productList.js
@@ -6,7 +6,57 @@ let Media;
 Media = ReactionCore.Collections.Media;
 Template.productList.helpers({
   products: function () {
-    return ReactionProducts.getProductsByTag(this.tag);
+    /*
+     * take natural sort, sorting by updatedAt
+     * then resort using positions.position for this tag
+     * retaining natural sort of untouched items
+     */
+
+    // function to compare and sort position
+    function compare(a, b) {
+      if (a.position.position === b.position.position) {
+        let x = a.position.updatedAt;
+        let y = b.position.updatedAt;
+
+        if (x > y) {
+          return -1;
+        } else if (x < y) {
+          return 1;
+        }
+
+        return 0;
+      }
+      return a.position.position - b.position.position;
+    }
+
+    let gridProducts = ReactionCore.Collections.Products.find({}).fetch();
+
+    for (let index in gridProducts) {
+      if ({}.hasOwnProperty.call(gridProducts, index)) {
+        let gridProduct = gridProducts[index];
+        if (gridProduct.positions) {
+          let _results = [];
+          for (let position of gridProduct.positions) {
+            if (position.tag === ReactionCore.getCurrentTag()) {
+              _results.push(position);
+            }
+            gridProducts[index].position = _results[0];
+          }
+        }
+        if (!gridProduct.position) {
+          gridProducts[index].position = {
+            position: 0,
+            weight: 0,
+            pinned: false,
+            updatedAt: gridProduct.updatedAt
+          };
+        }
+      }
+    }
+
+    const products = gridProducts.sort(compare);
+    Template.instance().products = products;
+    return products;
   },
   media: function () {
     let defaultImage;
