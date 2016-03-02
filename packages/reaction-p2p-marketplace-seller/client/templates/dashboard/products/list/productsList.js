56c1f96bbd08adeb0ddd967fc6a6ff7726f4bcac
ScyDev <lukas.saegesser@gmail.com>
2016-03-02 17:13:03 +0100

    - better backend products list with images

--- packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
+++ packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
@@ -1,42 +1,9 @@
 
-Template.dashboardProductsList.inheritsHelpersFrom("productGrid");
-Template.dashboardProductsList.inheritsEventsFrom("productGrid");
-Template.dashboardProductsList.inheritsHooksFrom("productGrid");
+Template.dashboardProductsList.inheritsHelpersFrom("productList"); // for media
+Template.dashboardProductsList.inheritsHooksFrom("productGrid"); // needed to make products show up
 
-/**
- * dashboardProductsList helpers
- *
- */
- /*
 Template.dashboardProductsList.helpers({
-  products: function (data) {
-    console.log("products: sreaching products for use "+Meteor.userId());
-    if (data.hash.data) {
-      return data.hash.data;
-    }
-    var productsFound = ReactionCore.Collections.Products.find({userId: Meteor.userId()}, {
-      sort: {
-        createdAt: -1
-      },
-      limit: 25
-    });
-
-    // copied
-    //let gridProducts = ReactionCore.Collections.Products.find({}).fetch();
-    let gridProducts = ReactionCore.Collections.Products.find({});
-
-    console.log("gridProducts: %o", gridProducts);
-    //const products = gridProducts.sort(compare);
-    Template.instance().products = gridProducts;
-    return gridProducts;
-    // /copied
-
-    console.log("products: %o", productsFound);
-    return productsFound;
+  products: function (data) { // override to show only this users products
+    return ReactionCore.Collections.Products.find({userId: Meteor.userId()});
   },
-  shopName: function () {
-    let shop = ReactionCore.Collections.Shops.findOne(this.shopId);
-    return shop !== null ? shop.name : void 0;
-  }
 });
-*/
