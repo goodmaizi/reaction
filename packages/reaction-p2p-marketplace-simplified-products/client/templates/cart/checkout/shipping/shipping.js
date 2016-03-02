2d2efe8fe7aff9aaf11dbc670b10eefc7c3b4f27
ScyDev <lukas.saegesser@gmail.com>
2016-03-02 13:59:40 +0100

    - timeout needed to make the click

--- packages/reaction-p2p-marketplace-simplified-products/client/templates/cart/checkout/shipping/shipping.js
+++ packages/reaction-p2p-marketplace-simplified-products/client/templates/cart/checkout/shipping/shipping.js
@@ -1,8 +1,11 @@
 
 Template.flatRateCheckoutShipping.onRendered(
   function() {
-    $(".checkout-shipping .list-group-item:nth-child(1)").trigger("click");
+    Meteor.setTimeout(function() {
+      $(".checkout-shipping .list-group-item:nth-child(1)").trigger("click");
+      console.log("clicked free shipping.");
 
-    //$("#main > div.checkout > div > div > div.checkout-steps-main > div:nth-child(3)").hide();
+      $("#main > div.checkout > div > div > div.checkout-steps-main > div:nth-child(3)").hide();
+    }, 100);
   }
 );
