749ec1074e2c6137d8806516a1dfaf227f25f990
ScyDev <lukas.saegesser@gmail.com>
2016-03-09 22:34:10 +0100

    - removed debug message

--- packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
+++ packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
@@ -6,7 +6,7 @@ Template.dashboardProductsList.helpers({
   products: function (data) { // override to show only this users products
     ReactionCore.Subscriptions.SellerProducts = ReactionSubscriptions.subscribe("SellerProducts");
     if (ReactionCore.Subscriptions.SellerProducts.ready()) {
-      console.log("helper Template.dashboardProductsList.helpers using publication SellerProducts.");
+      //console.log("helper Template.dashboardProductsList.helpers using publication SellerProducts.");
       return ReactionCore.Collections.Products.find({userId: Meteor.userId()});
     }
   },
