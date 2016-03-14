d62e86eed4b7d3b6863a9a6db082ca059a23d5eb
ScyDev <lukas.saegesser@gmail.com>
2016-03-14 13:14:13 +0100

    - made this hack a bit more robust since template may actually not be ready after 100ms, of course.

--- packages/reaction-p2p-marketplace-simplified-products/client/templates/cart/checkout/shipping/shipping.js
+++ packages/reaction-p2p-marketplace-simplified-products/client/templates/cart/checkout/shipping/shipping.js
@@ -1,11 +1,19 @@
 
 Template.flatRateCheckoutShipping.onRendered(
   function() {
-    Meteor.setTimeout(function() {
-      $(".checkout-shipping .list-group-item:nth-child(1)").trigger("click");
-      console.log("clicked free shipping.");
+    let autoClickFreeShipping = function() {
+      if ($("#main > div.checkout > div > div > div.checkout-steps-main > div:nth-child(3)").hasClass("checkout-step-completed")) {
+        Meteor.clearInterval(autoClickFreeShippingInterval);
+        console.log("removed interval.");
+      }
+      else {
+        $(".checkout-shipping .list-group-item:nth-child(1)").trigger("click");
+        console.log("clicked free shipping.");
 
-      $("#main > div.checkout > div > div > div.checkout-steps-main > div:nth-child(3)").hide();
-    }, 100);
+        $("#main > div.checkout > div > div > div.checkout-steps-main > div:nth-child(3)").hide();
+      }
+    }
+
+    let autoClickFreeShippingInterval = Meteor.setInterval(autoClickFreeShipping, 100);
   }
 );
