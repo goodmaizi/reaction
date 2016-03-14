778a47914ed595672f4e0af4a816c4854361e71c
ScyDev <lukas.saegesser@gmail.com>
2016-03-14 14:31:26 +0100

    - don't hide for the moment

--- packages/reaction-p2p-marketplace-simplified-products/client/templates/cart/checkout/shipping/shipping.js
+++ packages/reaction-p2p-marketplace-simplified-products/client/templates/cart/checkout/shipping/shipping.js
@@ -10,7 +10,7 @@ Template.flatRateCheckoutShipping.onRendered(
         $(".checkout-shipping .list-group-item:nth-child(1)").trigger("click");
         console.log("clicked free shipping.");
 
-        $("#main > div.checkout > div > div > div.checkout-steps-main > div:nth-child(3)").hide();
+        //$("#main > div.checkout > div > div > div.checkout-steps-main > div:nth-child(3)").hide();
       }
     }
 
