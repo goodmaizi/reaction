9a90b43f9d1a7217b18451ee733f9c456185e55d
ScyDev <lukas.saegesser@gmail.com>
2016-03-21 17:53:31 +0100

    - just desc for seller package

--- packages/reaction-p2p-marketplace-seller/package.js
+++ packages/reaction-p2p-marketplace-seller/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'scydev:reaction-p2p-marketplace-seller',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Allow users to be sellers.',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
