a5d3abb11d48c89255180818ac88163daf457c41
ScyDev <lukas.saegesser@gmail.com>
2016-04-06 15:21:15 +0200

    - apply seller CSS only if product belongs to current user

--- packages/reaction-p2p-marketplace-seller/client/templates/products/productDetail/productDetail.js
+++ packages/reaction-p2p-marketplace-seller/client/templates/products/productDetail/productDetail.js
@@ -47,6 +47,6 @@ Template.registerHelper("belongsToCurrentUser", function (productId) {
 
   let productBelongingToCurrUser = ReactionCore.Collections.Products.findOne({_id:productId, userId:Meteor.userId()})
   console.log("Template.helpers.belongsToCurrentUser() Product ",productId," belongs to ",Meteor.userId(),"?");
-  //console.log("Template.helpers.belongsToCurrentUser() productBelongingToCurrUser ",productBelongingToCurrUser);
+  console.log("Template.helpers.belongsToCurrentUser() productBelongingToCurrUser ",productBelongingToCurrUser);
   return productBelongingToCurrUser != null;
 });
