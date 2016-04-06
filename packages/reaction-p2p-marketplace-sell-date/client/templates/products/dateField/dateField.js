381c6d3cb732a11bb51427279a3103fd68ad7230
ScyDev <lukas.saegesser@gmail.com>
2016-04-06 17:13:14 +0200

    - getting a bit crazy with datepicker init...

--- packages/reaction-p2p-marketplace-sell-date/client/templates/products/dateField/dateField.js
+++ packages/reaction-p2p-marketplace-sell-date/client/templates/products/dateField/dateField.js
@@ -1,34 +1,52 @@
 
 // inherit helpers from template productDetail so we can use fieldComponent in this here template
 
+function initDatepickers() {
+  $(".forSaleOnDate-edit-input").datepicker({
+    format: "dd.mm.yyyy"
+  });
+
+  $('.datetimepicker').datetimepicker({
+    format: "DD.MM.YYYY hh:mm", //
+    locale: moment.locale("de"),
+    //sideBySide: true
+  });
+
+  $('.datetimepicker').on("dp.change", function(event) {
+    console.log("datetimepicker changed: ",event.date);
+    let fixedDatetime = event.date.subtract(2, 'hours'); // for some reason the event.date is 2 hours diffwerent from what we choose and see in the input field
+    $('.latestOrderDate-edit-input').val(fixedDatetime.format("DD.MM.YYYY hh:mm"));
+    $('.latestOrderDate-edit-input').trigger("change");
+  });
+  // set date from real input field
+  $('.latestOrderDate-dummy-input').val($('.latestOrderDate-edit-input').val());
+
+  console.log("activated datepicker");
+}
+
 Template.productDetailDateField.inheritsHelpersFrom(["productDetail", "productDetailEdit"]);
 Template.productDetailDateField.inheritsEventsFrom(["productDetail", "productDetailEdit"]);
 Template.productDetailDateField.inheritsHooksFrom(["productDetail", "productDetailEdit"]);
 
+Template.productDetailDateField.onCreated(
+  function() {
+    Template.instance().autorun(function() {
+      //initDatepickers();
+    });
+  }
+);
+
 Template.productDetailDateField.onRendered(
   function() {
     Meteor.setTimeout(function() { // what the?!? document doesn't seem to be ready immediately when this event is fired...
-      $(".forSaleOnDate-edit-input").datepicker({
-        format: "dd.mm.yyyy"
-      });
-
-      $('.datetimepicker').datetimepicker({
-        format: "DD.MM.YYYY hh:mm", //
-        locale: moment.locale("de"),
-        //sideBySide: true
-      });
-      //$('.datetimepicker').data("DateTimePicker").change(function(event) {
-      $('.datetimepicker').on("dp.change", function(event) {
-        console.log("datetimepicker changed: ",event.date);
-        let fixedDatetime = event.date.subtract(2, 'hours'); // for some reason the event.date is 2 hours diffwerent from what we choose and see in the input field
-        $('.latestOrderDate-edit-input').val(fixedDatetime.format("DD.MM.YYYY hh:mm"));
-        $('.latestOrderDate-edit-input').trigger("change");
-      });
-      // set date from real input field
-      $('.latestOrderDate-dummy-input').val($('.latestOrderDate-edit-input').val());
-
-      console.log("activated datepicker");
+      initDatepickers();
     }, 100);
+    Meteor.setTimeout(function() { // what the?!? document doesn't seem to be ready immediately when this event is fired...
+      initDatepickers();
+    }, 1000);
+    Meteor.setTimeout(function() { // what the?!? document doesn't seem to be ready immediately when this event is fired...
+      initDatepickers();
+    }, 2000);
   }
 );
 
