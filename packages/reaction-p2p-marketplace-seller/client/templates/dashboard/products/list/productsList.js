45dd96ad9aa79b09b2837ed12a9e2c9d116c4cdb
ScyDev <lukas.saegesser@gmail.com>
2016-04-13 15:57:22 +0200

    - don't use ReactionSubscriptions (SubscriptionsManager) for SellerProducts, so that extended publication will only be used on sellers products page and nowhere else

--- packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
+++ packages/reaction-p2p-marketplace-seller/client/templates/dashboard/products/list/productsList.js
@@ -5,8 +5,8 @@ Template.dashboardProductsList.inheritsHelpersFrom("gridContent"); // for price
 
 Template.dashboardProductsList.helpers({
   products: function (data) { // override to show only this users products
-    ReactionCore.Subscriptions.SellerProducts = ReactionSubscriptions.subscribe("SellerProducts");
-    if (ReactionCore.Subscriptions.SellerProducts.ready()) {
+    let SellerProducts = Meteor.subscribe("SellerProducts");
+    if (SellerProducts.ready()) {
       //console.log("helper Template.dashboardProductsList.helpers using publication SellerProducts.");
       return ReactionCore.Collections.Products.find({userId: Meteor.userId()});
     }
